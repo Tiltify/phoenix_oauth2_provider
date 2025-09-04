2843a9b233b6e14b3cf9da8e4828819bf8808e34
Adam Malosh <adam.moolash@gmail.com>
2025-09-04 10:39:54 -0700

    fix: only use html format

--- lib/phoenix_oauth2_provider/controller.ex
+++ lib/phoenix_oauth2_provider/controller.ex
@@ -7,7 +7,7 @@ defmodule PhoenixOauth2Provider.Controller do
   @doc false
   defmacro __using__(type) do
     quote do
-      use Phoenix.Controller 
+      use Phoenix.Controller, formats: [:html]
 
       alias PhoenixOauth2Provider.Router.Helpers, as: Routes
 
@@ -66,7 +66,7 @@ defmodule PhoenixOauth2Provider.Controller do
   end
 
   defp put_view(conn, nil), do: conn
-  defp put_view(%{private: %{phoenix_view: phoenix_view}} = conn, web_module) do
+  defp put_view(%{private: %{phoenix_view: %{_: phoenix_view}}} = conn, web_module) do
     view_module = Module.concat([web_module, phoenix_view])
 
     Phoenix.Controller.put_view(conn, view_module)
